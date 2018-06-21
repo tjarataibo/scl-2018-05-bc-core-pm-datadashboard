e6e5493f329d32c0d92bc748ff05f3687b5ea8c5
Camila <camipereira2111@gmail.com>
2018-06-21 14:32:14 -0400

    Elimino HTML alumnas y cohort, cambios en index.html y data.js

--- src/data.js
+++ src/data.js
@@ -1,58 +1,110 @@
-window.onload
-
-//Función para users
-async function fetchAsync() {
-    //Se hace una petición con let response = await fetch ('URL');
-
-    const responseOne = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json');
-    //Se manda una respuesta como let data = await response.json();
-    const users = await responseOne.json();
-
-    return users;
+function users() {
+    const urlUsers = "https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json"
+    fetch(urlUsers)
+    .then(response => response.json())
+    .then(data => { 
+      console.log(data);
+    })
 }
 
-  fetchAsync()
-    //Cuando se cumple
-    .then(users => console.log(users))
-    //Cuando no se cumple
-    .catch(error => console.log(error))
+const searchUsers = document.getElementsByClassName("searchByNAme");
+const urlUsers = "https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json"
+fetch (urlUsers).then(
+    (response)=>{
+        if(response.ok){
+            return response.json();               
+        }
+    }
+).then((responseJsons)=>{ //Arreglo de respuestas en json
+        /*
+         * Código que ocupa los jsons...
+         */
+        console.log(urlUsers);
+    })
+
+    const urlProgress ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json" 
+    fetch (urlProgress).then(
+        (response)=>{
+            if(response.ok){
+                return response.json();                
+            }
+        }
+    ).then((responseJsons)=>{ //Arreglo de respuestas en json
+            /*
+             * Código que ocupa los jsons...
+             */
+            console.log(urlProgress);
+        })    
+
+        const urlCohorts ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json"
+        fetch (urlCohorts).then(
+            (response)=>{
+                if(response.ok){
+                    return response.json();                
+                }
+            }
+        ).then((responseJsons)=>{ //Arreglo de respuestas en json
+                /*
+                 * Código que ocupa los jsons...
+                 */
+                console.log(urlCohorts);
+            })        
+
+// window.onload
+
+// //Función para users
+// async function fetchAsync() {
+//     //Se hace una petición con let response = await fetch ('URL');
+
+//     const responseOne = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json');
+//     //Se manda una respuesta como let data = await response.json();
+//     const users = await responseOne.json();
+
+//     return users;
+// }
+
+//   fetchAsync()
+//     //Cuando se cumple
+//     .then(users => console.log(users))
+//     //Cuando no se cumple
+//     .catch(error => console.log(error))
 
 
-//Función para progress
-async function fetchAsyncTwo() {
-    const responseTwo = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json');
-    const progress = await responseTwo.json();
+// //Función para progress
+// async function fetchAsyncTwo() {
+//     const responseTwo = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+//     const progress = await responseTwo.json();
 
-    return progress;
-}
-
-
- fetchAsyncTwo()
-    //Cuando se cumple
-    .then(progress => console.log(progress))
-    //Cuando no se cumple
-    .catch(error => console.log(error))
+//     return progress;
+// }
 
 
-//Función para cohorts
-async function fetchAsyncThree() {
-    const responseThree = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json');
-    const cohorts = await responseThree.json();
-
-    return cohorts;
-}
+//  fetchAsyncTwo()
+//     //Cuando se cumple
+//     .then(progress => console.log(progress))
+//     //Cuando no se cumple
+//     .catch(error => console.log(error))
 
 
-fetchAsyncThree()
-//Cuando se cumple
-.then(cohorts => console.log(cohorts))
-//Cuando no se cumple
+// //Función para cohorts
+// async function fetchAsyncThree() {
+//     const responseThree = await fetch('https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json');
+//     const cohorts = await responseThree.json();
 
-.catch(error => console.log(error))
+//     return cohorts;
+// }
 
 
-//Llamar a las funciones con botones
+// fetchAsyncThree()
+// //Cuando se cumple
+// .then(cohorts => console.log(cohorts))
+// //Cuando no se cumple
 
-document.getElementById("Alumnas").addEventListener("click", fetchAsync() )
+// .catch(error => console.log(error))
+
+
+// //Llamar a las funciones con botones
+
+// document.getElementById("Alumnas").addEventListener("click", fetchAsync() )
 
 
