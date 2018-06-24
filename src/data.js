bdd7b71d025c9aa85786c5e7f12ff281ea4b4cef
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-06-24 19:32:39 -0300

    subo en limpio funcion promisse all con precaria visualizacion en html

--- src/data.js
+++ src/data.js
@@ -9,58 +9,78 @@ document.getElementById("img").style.visibility="hidden";
 */
 
 
+////////////////////////////////////////////////////////////////////////////
+function verTabla()
+{
+document.getElementById("frm").style.visibility="visible";
+ 
+document.getElementById("img").style.visibility="hidden";
+ 
+}
+
+function tabla(responseJsons) {
+    //console.log(datos)
+    document.getElementById("contenido").innerHTML = ""
+    for (let valor of responseJsons) {
+        //console.log(valor)
+        document.getElementById("contenido").innerHTML += `
+        
+        <tr>
+        <th> ${valor.id}</th>
+        
+      </tr>
+        `
+    }
+
+} 
+
 function traer() {
-    //se declaran variables para las url donde se alojan las datas
 
     const urlUsers = "https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json";
     const urlProgress ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json"; 
     const urlCohorts ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json";
+    
 
-    Promise.all([   //Ejecuta todas las llamadas de manera paralela
+    Promise.all([   //llama todas las bases
 
     fetch(urlUsers),
     fetch(urlProgress),
     fetch(urlCohorts),
     
-    ])
-    .then((responses)=>{   //Responde a todas las promesas
-            return Promise.all(responses.map((response)=>{
-                //console.log(response);
-                return response.json();                
-            }));                       
-        }               
+    ]) .then((responses)=>{   //Responde a todas las promesas
+        return Promise.all(responses.map((response)=>{ //map me entrega un nuevo arreglo con las tres bases
+            //console.log(response);
+            return response.json();                
+        }));                       
+    }               
     
 
-    )
-    .then((responseJsons)=>{ //Arreglo de respuestas en json
-        console.log(responseJsons)    
-        //tabla(responseJsons)
-    /*
+    ).then((responseJsons) =>{ //Arreglo de las respuestas en json
+       let users = responseJsons[0]
+       let progress = responseJsons[1]
+       let cohorts = responseJsons[2].filter((cohort)=>{
+          
+           return cohort.id;
+           
+       });
+
+       
+
+       tabla(responseJsons)
+     //console.log(responseJsons)
+     
+     
+        /*
      * Código que ocupa los jsons...
      */
     })
-    
+   
     .catch(
     (error)=>{ // Al menos una llamada falló
 
     }
 )}
-//promisse all listo !!! HASTA ACA FUNCIONA CSM!!!!!
+//promisse all listo !!! HASTA ACA FUNCIONA !!!!!
 
 
-    /*function tabla(datos) {
-        //console.log(datos)
-        document.getElementById("contenido").innerHTML = ""
-        for (let valor of datos) {
-            console.log(valor)
-            document.getElementById("contenido").innerHTML += `
-            <tr>
-            <th scope="row">${valor.id}</th>
-            <td>${valor.name}</td>
-            <td>${valor.signupCohort}</td>
-            <td>${valor.timezone}</td>
-          </tr>
-            `
-        }
     
-    }     comento funciòn tabla para ocuparla despues*/ 
