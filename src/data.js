8505e7b1a51617e7f2aab64235f45eb0161e0f52
Tamara <tjarataibo@gmail.com>
2018-06-24 20:47:43 -0400

    Orden para js

--- src/data.js
+++ src/data.js
@@ -21,59 +21,54 @@ function tabla(responseJsons) { //tabla para mostrar llamado de datas
     }
 
 } 
+const users = "https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json";
+const progress ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json"; 
+const cohorts ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json";
 
-function traer() {
 
-    const urlUsers = "https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json";
-    const urlProgress ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json"; 
-    const urlCohorts ="https://tjarataibo.github.io/scl-2018-05-bc-core-pm-datadashboard/data/cohorts.json";
-    
+//se declaran variables para las url donde se alojan las datas
 
-    Promise.all([   //llama todas las bases
 
-    fetch(urlUsers),
-    fetch(urlProgress),
-    fetch(urlCohorts),
-    
-    ]) .then((responses)=>{   //Responde a todas las promesas
-        return Promise.all(responses.map((response)=>{ //map me entrega un nuevo arreglo con las tres bases
+Promise.all([   //Ejecuta todas las llamadas de manera paralela
+
+fetch(users),
+fetch(progress),
+fetch(cohorts),
+
+])
+.then((responses)=>{   //Responde a todas las promesas
+        return Promise.all(responses.map((response)=>{
             //console.log(response);
             return response.json();                
         }));                       
     }               
+
+
+)
+.then((responseJsons)=>{ //Arreglo de respuestas en json
+    let users = responseJsons[0]
+    let progress = responseJsons[1]
+    let cohorts = responseJsons[2].filter((cohort)=>{
+      return cohort.id 
+  });
+        console.log(cohorts);    
+        console.log(users);
+        console.log(progress);
+        
     
-
-    ).then((responseJsons) =>{ //Arreglo de las respuestas en json
-       let users = responseJsons[0]
-       let progress = responseJsons[1]
-       let cohorts = responseJsons[2].filter((cohort)=>{
-          
-           return cohort.id;
-           
-       });
-
-       
-
-       tabla(responseJsons)
-     //console.log(responseJsons)
+    // courses.id = object.keys(cohorts[2].coursesIndex);
+    result = computeUsersStats(users,progress,courses);
+    sortUsers(result,"name","DESC");
      
-     
-        /*
-     * Código que ocupa los jsons...
-     */
-    })
-   
-    .catch(
-    (error)=>{ // Al menos una llamada falló
 
-    }
-)}
-//promisse all listo !!! HASTA ACA FUNCIONA !!!!!
+})
 
+.catch(
+(error)=>{ // Al menos una llamada falló
 
-
-//                  <------------------------->
-
+}
+)
+//promisse all listo !!! HASTA ACA FUNCIONA.
 
 
 // 1) computeUsersStats(users, progress, courses)
